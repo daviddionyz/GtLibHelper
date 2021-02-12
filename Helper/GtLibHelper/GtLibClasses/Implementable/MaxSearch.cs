eca33cbdcd088b44fd7b7ae48446cbafb52d0fd5
Dionyz David <david.dionyz@foxpost.hu>
2021-02-12 11:02:41 +0100

    adding maxsearch

--- Helper/GtLibHelper/GtLibClasses/Implementable/MaxSearch.cs
+++ Helper/GtLibHelper/GtLibClasses/Implementable/MaxSearch.cs
@@ -6,5 +6,45 @@ namespace GtLibHelper.GtLibClasses.Implementable
 {
     public class MaxSearch : AbstractLibClass
     {
+        public MaxSearch(int ID, string name)
+        {
+            this.ID = ID;
+            this.Name = name;
+            this.Type = "MaxSearch";
+            this.NeededHeader = "include \"maxsearch.hpp\"";
+            this.Text = $"class {name}" + " : public MaxSearch<Item, Value, Compare>\r\n" +
+                "{ \r\n" +
+                "private: \r\n" +
+                "   \r\n" +
+                "protected: \r\n" +
+                "   Value func(const Item& e) const override { };\r\n" +
+                "   bool  cond(const Item& e) const override { return true;}\r\n" +
+                "public: \r\n" +
+                "   bool found()   const { return _l;}\r\n" +
+                "   Value opt()    const { return _opt;}\r\n" +
+                "   Item optElem() const { return _optelem;}\r\n" +
+                "}\r\n";
+        }
     }
 }
+
+// template <typename Item, typename Value, typename Compare >
+// class MaxSearch : public Procedure<Item, Value, Compare>
+// {
+// protected:
+//     bool    _l;
+//     Item    _optelem;
+//     Value   _opt;
+//     Compare _better;
+
+//     void init() final override { _l = false;}
+//     void body(const Item& e) final override;
+
+//     virtual Value func(const Item& e) const = 0;
+//     virtual bool  cond(const Item& e) const { return true;}
+
+// public:
+//     bool found()   const { return _l;}
+//     Value opt()    const { return _opt;}
+//     Item optElem() const { return _optelem;}
+// };
--- Helper/GtLibHelper/GtLibClasses/Implementable/Summation.cs
+++ Helper/GtLibHelper/GtLibClasses/Implementable/Summation.cs
@@ -17,9 +17,9 @@ namespace GtLibHelper.GtLibClasses.Implementable
                 "private: \r\n" +
                 "\r\n" +
                 "public: \r\n" +
-                "T func(const A& e) const override {}\r\n" +
-                "T netural() const override {}\r\n" +
-                "T add( const A& a, const A& b) const override {}\r\n" +
+                "   T func(const A& e) const override {}\r\n" +
+                "   T netural() const override {}\r\n" +
+                "   T add( const A& a, const A& b) const override {}\r\n" +
                 "} \r\n";
         }
     }
